4f04077054e975093abfe80feb15ab906234d209
ronsun <ron.sun@hotmail.com.tw>
2021-06-06 01:19:15 +0800

    AggregationLib version 1

---
+++ AggregationLib/Worker.cs
@@ -0,0 +1,17 @@
+﻿using BaseLib;
+
+namespace AggregationLib
+{
+    public static class Worker
+    {
+        public static string GetBaseLibVersion()
+        {
+            return VersionInfo.CurrentVersion;
+        }
+
+        public static string GetBaseLibV1Description()
+        {
+            return VersionInfo.V1;
+        }
+    }
+}
