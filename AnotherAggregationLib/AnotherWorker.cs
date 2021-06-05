7fa0bdc18dca9e6c7b96ba927fdbed8074e0b5b5
ronsun <ron.sun@hotmail.com.tw>
2021-06-06 02:30:24 +0800

    AnotherAggregationLib version 1

---
+++ AnotherAggregationLib/AnotherWorker.cs
@@ -0,0 +1,17 @@
+﻿using BaseLib;
+
+namespace AnotherAggregationLib
+{
+    public class AnotherWorker
+    {
+        public static string GetBaseLibVersion()
+        {
+            return VersionInfo.CurrentVersion;
+        }
+
+        public static string GetBaseLibV1Description()
+        {
+            return VersionInfo.V1;
+        }
+    }
+}
