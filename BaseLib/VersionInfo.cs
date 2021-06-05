5207a05113f0a4f98c5c48ee4ff75f2d7bcf1400
ronsun <ron.sun@hotmail.com.tw>
2021-06-06 01:07:06 +0800

    BaseLIb V2

--- BaseLib/VersionInfo.cs
+++ BaseLib/VersionInfo.cs
@@ -4,8 +4,8 @@ namespace BaseLib
 {
     public static class VersionInfo
     {
-        public static string CurrentVersion => "1.0.0";
+        public static string CurrentVersion => "2.0.0";
 
-        public static string V1 => "only for V1";
+        public static string V2 => "only for V2";
     }
 }
